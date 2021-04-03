380cb52baf10dcbc881682e63a134c6c21086929
Nickolay78 <comcriminal@gmail.com>
2021-04-03 17:00:24 +0300

    Firsr Part Hist

---
+++ 1.R
@@ -0,0 +1,8 @@
+
+outcome[, 11] <- as.numeric(outcome[, 11])
+## You may get a warning about NAs being introduced; that is okay
+hist(outcome[, 11])
+
+## Важно, если естественный процесс, значит такое должно 
+## быть и в милицейской статистике
+## Может это и ответ. Где в в статистике РНОРМ
